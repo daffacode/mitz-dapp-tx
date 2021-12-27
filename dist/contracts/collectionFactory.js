a99f0d1bd1937866526a7f707754f630a4178318
Daffa <84918684+daffacode@users.noreply.github.com>
2021-12-27 15:48:00 +0700

    dist

---
+++ dist/contracts/collectionFactory.js
@@ -0,0 +1,33 @@
+import { ChainId } from '@mitz/schems';
+import { abis } from '../abis';
+export const collectionFactory = {
+    [ChainId.ETHEREUM_ROPSTEN]: {
+        version: '2',
+        abi: abis.CollectionFactory,
+        address: '0x16d8bac5b67a6b782a9081377bec413bc5bb56a6',
+        name: 'CollectionFactory',
+        chainId: ChainId.ETHEREUM_ROPSTEN
+    },
+    [ChainId.HARMONY_TESTNET]: {
+        version: '2',
+        abi: abis.CollectionFactory,
+        address: '0x935eb73683971b513779da1637980545cd208afb',
+        name: 'CollectionFactory',
+        chainId: ChainId.HARMONY_TESTNET
+    },
+    [ChainId.MATIC_MUMBAI]: {
+        version: '2',
+        abi: abis.CollectionFactory,
+        address: '0x2A72Ec4241Ac4fBc915ae98aC5a5b01AdE721f4B',
+        name: 'CollectionFactory',
+        chainId: ChainId.MATIC_MUMBAI
+    },
+    [ChainId.MATIC_MAINNET]: {
+        version: '2',
+        abi: abis.CollectionFactory,
+        address: '0xB549B2442b2BD0a53795BC5cDcBFE0cAF7ACA9f8',
+        name: 'CollectionFactory',
+        chainId: ChainId.MATIC_MAINNET
+    }
+};
+//# sourceMappingURL=collectionFactory.js.map
---
+++ dist/contracts/erc20.js
@@ -0,0 +1,61 @@
+import { ChainId } from '@mitz/schems';
+import { abis } from '../abis';
+export const erc20 = {
+    [ChainId.MATIC_MUMBAI]: {
+        version: '2',
+        abi: abis.ERC20,
+        address: '',
+        name: 'ERC20',
+        chainId: ChainId.MATIC_MUMBAI
+    },
+    [ChainId.HARMONY_TESTNET]: {
+        version: '2',
+        abi: abis.ERC20,
+        address: '',
+        name: 'ERC20',
+        chainId: ChainId.HARMONY_TESTNET
+    },
+    [ChainId.MATIC_MAINNET]: {
+        version: '2',
+        abi: abis.ERC20,
+        address: '',
+        name: 'ERC20',
+        chainId: ChainId.MATIC_MAINNET
+    },
+    [ChainId.ETHEREUM_MAINNET]: {
+        version: '2',
+        abi: abis.ERC20,
+        address: '',
+        name: 'ERC20',
+        chainId: ChainId.ETHEREUM_MAINNET
+    },
+    [ChainId.ETHEREUM_ROPSTEN]: {
+        version: '2',
+        abi: abis.ERC20,
+        address: '',
+        name: 'ERC20',
+        chainId: ChainId.ETHEREUM_ROPSTEN
+    },
+    [ChainId.ETHEREUM_RINKEBY]: {
+        version: '2',
+        abi: abis.ERC20,
+        address: '',
+        name: 'ERC20',
+        chainId: ChainId.ETHEREUM_RINKEBY
+    },
+    [ChainId.ETHEREUM_KOVAN]: {
+        version: '2',
+        abi: abis.ERC20,
+        address: '',
+        name: 'ERC20',
+        chainId: ChainId.ETHEREUM_KOVAN
+    },
+    [ChainId.ETHEREUM_GOERLI]: {
+        version: '2',
+        abi: abis.ERC20,
+        address: '',
+        name: 'ERC20',
+        chainId: ChainId.ETHEREUM_GOERLI
+    }
+};
+//# sourceMappingURL=erc20.js.map
